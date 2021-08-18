54955b89794ebbb9ce3ce086a4de93f3af6494f0
Neringa Sungailaite <n.sungailaite@gmail.com>
2021-08-18 19:52:14 +0300

    update with get/id

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-const client = new MongoClient(process.env.uri);
+const URI = `mongodb+srv://dbAdmin:${process.env.MONGODBPW}@clusterdemo.e4wnm.mongodb.net/E-shop?retryWrites=true&w=majority`;
+const client = new MongoClient(URI);
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -16,6 +17,22 @@ app.get('/products', async (req, res) => {
     res.send(data);
   } catch (e) {
     res.status(500).send({ error: ' Please try again' });
+  }
+});
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con
+      .db('E-shop')
+      .collection('Products')
+      .find(ObjectId(req.params.id))
+      .toArray();
+    await con.close();
+
+    return res.send(data);
+  } catch (err) {
+    return res.status(500).send({ err });
   }
 });
 
