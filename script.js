3bdff804865bb73201689a99a2b26a577610f6a9
Neringa Sungailaite <n.sungailaite@gmail.com>
2021-08-18 18:51:54 +0300

    update

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
+const uri =
+  'mongodb+srv://dbAdmin:<password>@clusterdemo.e4wnm.mongodb.net/E-shop?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.get('/', (req, res) => {
-  res.send({ msg: 'Success' });
+
+app.get('/products', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db('E-shop').collection('Products').find().toArray();
+    await con.close();
+    return res.send(data);
+  } catch (e) {
+    return res.status(500).send({ error: ' Please try again' });
+  }
 });
 
 const port = process.env.PORT || 3000;
